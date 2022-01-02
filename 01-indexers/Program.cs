bd7d05abe144db06b448eb5dcb47631548f65253
VoN <sudo.von.contact@gmail.com>
2022-01-01 18:49:55 -0600

    feat: :sparkles: Add indexers

---
+++ 01-indexers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _01_indexers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var indexer = new Indexer();
+            indexer[0] = 1;
+            indexer[1] = 2;
+            indexer[2] = 3;
+            Console.WriteLine(indexer[0]);
+        }
+    }
+
+    class Indexer
+    {
+        private int[] list = new int[100];
+        public int this [int i]
+        {
+            get
+            {
+                return list[i];
+            }
+            set
+            {
+                list[i] = value;
+            }
+        }
+    }
+}
